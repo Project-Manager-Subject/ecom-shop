0376beca15eb7ef171d07e70fc4b10cdf9df6cb4
dinhhuuphi <dinhhuuphi502@gmail.com>
2022-10-14 15:49:06 +0700

    Phi add login

--- blog/app/views/user_login.php
+++ blog/app/views/user_login.php
@@ -1,1 +1,18 @@
 <!-- Bổ sung code tại đây -->
+<?php 
+    $email = "";
+    $password = "";
+    $check = false;
+    if(isset($_COOKIE['email']) && isset($_COOKIE['password'])){
+        $email = $_COOKIE['email'];
+        $password = $_COOKIE['password'];
+        $check = true;
+    }
+
+?>
+<div class="container" style="margin-top: 20px;">
+    <div class="col-md-6">
+        <?php
+          if(!empty())
+    </div>
+</div>
