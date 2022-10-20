7b3db83f102586de9be311b928e8ae10770bf4d9
hung1127 <nguyenthanhhung200111@gmail.com.vn>
2022-10-20 22:18:31 +0700

    login

--- blog/app/views/user_signup.php
+++ blog/app/views/user_signup.php
@@ -1,4 +1,4 @@
-<div class="contsiner" style="margin-top: 20px;">
+<div class="container" style="margin-top: 20px;">
     <div class="col-md-6">
     <?php
         if(!empty($_GET['msg'])){
@@ -9,26 +9,26 @@
         }
         ?>
         <h1 style ="margin: 15px 0;">Đăng Ký Tài Khoản</h1>
-        <form action="<?php echo  BASE_URL ?>/user_login/auth_signup"method="POST">
+        <form class = "form-group" action="<?php echo  BASE_URL ?>/user_login/auth_signup"method="POST">
         <div class="form-group">
-            <label>Tên Khách Hàng:</lable>
-            <input type="text" name="name" class="form-control" required>
+            <label for="">Tên khách hàng</label>
+            <input type="text" name="name"  class="form-control" required>
         </div>
         <div class="form-group">
-            <label>Email:</lable>
+        <label for="">Email</label>
             <input required  type="email" name="email" class="form-control" >
         </div>
         <div class="form-group">
-            <label>Password:</lable>
+        <label for="">Password</label>
             <input required  type="password" name="password" class="form-control" >
         </div>
         <div class="form-group">
-            <label>Số Điện Thoại:</lable>
+        <label for="">Số điện thoại</label>
             <input required  type="text" name="phone" class="form-control" >
         </div>
         <div class="form-group">
-            <label>Địa Chỉ Nhận Hàng:</lable> <br>
-            <label for="tinh">Tỉnh</lable> 
+        <label style="font-size: 15px; margin-bottom:5px;" for="">Địa chỉ nhận hàng:</label> <br>
+        <label for="tinh">Tỉnh</label> 
             <select name="tinh" class="form-control" id="tinh" required>
                 <option value="">--Tỉnh--</option> 
                 <?php foreach($tinh as $key=>$value):?>
@@ -38,12 +38,14 @@
             </select>
         </div>
         <div class="form-group">
-            <label for="quanhuyen">Quận/Huyện</lable> 
+            
+            <label for="quanhuyen">Quận/Huyện</label>
             <select name="quanhuyen" class="form-control" id="quanhuyen" required>
             </select>
         </div>
         <div class="form-group">
-            <label for="quanhuyen">Xã/Phường/Thị Trấn</lable> 
+            
+            <label for="quanhuyen">Xã/Phường/Thị trấn</label> 
             <select name="xa" class="form-control" id="xa" required>
             </select>
         </div>
