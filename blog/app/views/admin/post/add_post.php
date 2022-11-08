ac7459cb6f8c35ec0a7297067c51e87a6e06559f
= <=>
2022-11-08 09:08:22 +0700

    ykhoa_build_add_post

--- blog/app/views/admin/post/add_post.php
+++ blog/app/views/admin/post/add_post.php
@@ -1,1 +1,27 @@
-<!-- Bổ sung code tại đây -->
+<h2 class="heading-section">Add Post</h2>
+<form action="<?php echo BASE_URL ?>/post/insert_post" method="POST" enctype="multipart/form-data">
+  <div class="class-group">
+    <label for="">Name:</label>
+    <input type="text" class="form-control" placeholder="Title" name="title" require>
+  </div>
+  <div class="class-group">
+    <label for="">Desc</label>
+    <textarea name="desc" class="form-control" id="" cols="30" rows="10"></textarea>
+  </div>
+  <div class="class-group">
+    <label for="">Image:</label>
+    <input type="file" class="form-control" placeholder="Enter Quantity" name="file" require>
+  </div>
+  <div class="class-group">
+    <label for="">Category</label>
+    <select name="category" class="form-control">
+      <?php if(!empty($category)) :
+          foreach ($category as $key => $value) :
+       ?>
+        <option value="<?php echo $value['id_category_post'] ?>"><?php echo $value['title_category_post']?></option>
+        <?php endforeach ?>
+      <?php endif ?>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary btn-submit" name="insert">Submit</button>
+</form>
